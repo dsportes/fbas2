d1f8e55202479bdd59bb0604a6e2b62fcb1fb5ad
Daniel Sportes <daniel@sportes.fr>
2021-04-19 21:11:33 +0200

    config.json sorti

--- src/boot/serverConnection.js
+++ src/boot/serverConnection.js
@@ -1,13 +1,14 @@
 // import something here
 import firebaseService from '../services/firebase'
+import axios from 'axios'
 
 // "async" is optional;
 // more info on params: https://v2.quasar.dev/quasar-cli/boot-files
 export default async (/* { app, router, ... } */) => {
   // something to do
-  console.log('Boot ... ' + process.env.environments.ENV_TYPE)
-  const config = process.env.environments.FIREBASE_CONFIG
-  const app = firebaseService.fBInit(config)
+  const config = await axios.get('./config.json')
+  console.log('Boot ... ' + process.env.MODE)
+  const app = firebaseService.fBInit(config.data)
 
   // Validation that our service structure is working
   // with a Firebase application auth instance. Does not validate a
